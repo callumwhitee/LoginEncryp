af40fda4f38445131dc00fdeb9c7b8ff96a6c4b6
NEO <cwitenice@gmail.com>
2021-11-17 22:58:51 +0000

    Enctryption Sorted ;)

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
 ﻿using System;
-
 class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        var PasswordHasher = new Hash();
+        Console.WriteLine("Enter Four Passwords");
+        for(int i = 0; i < 4; i ++)
+        {
+            Console.Write("> ");
+            string a = Console.ReadLine();
+            var hash = PasswordHasher.hash(a, 10000);
+            Console.WriteLine(hash);
+        }
+
     }
 }
